e59e9ca35d1611f83b4a4084921a4fbc1872dc0b
tuweile <tuweile97@hotmail.com>
2022-10-06 23:48:45 +0800

    Updated fixed_yields.py

--- script/fixed_yields.py
+++ script/fixed_yields.py
@@ -13,15 +13,15 @@ class FixedBondYields:
             eer = [0 for i in range(10)]
 
             # Obtain the simple average of the daily 1 Year SGS benchmark yields for that month
-            eer[0] = round((sum(data[3]) / len(data[3])), 2)
+            eer[0] = (sum(data[3]) / len(data[3]))
 
             # Obtain the simple average of the daily 2 Year SGS benchmark yields for that month
-            eer[1] = round((sum(data[4]) / len(data[4])), 2)
+            eer[1] = (sum(data[4]) / len(data[4]))
 
             # Obtain the simple average of the daily 5 Year SGS benchmark yields for that month
-            eer[4] = round((sum(data[5]) / len(data[5])), 2)
+            eer[4] = (sum(data[5]) / len(data[5]))
 
             # Obtain the simple average of the daily 10 Year SGS benchmark yields for that month
-            eer[-1] = round((sum(data[6]) / len(data[6])), 2)
+            eer[-1] = (sum(data[6]) / len(data[6]))
 
             return eer
